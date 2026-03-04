d18b5b853e90616b4e353e5a3baf6af8becaabdd
cy-2-u <18228658985@163.com>
2026-03-04 16:42:28 +0800

    Update default values for CFIP and NAME

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'zzz.zyyunchun.us.ci';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZGZiOGJjZmQ0YTQ5NTZjZWQ1ZTIyNjA1NjVjYTFiY2EiLCJ0IjoiZDYwMTM0NTgtMzc0NS00NGUzLWI3MTAtNTM1NjY0YmJhMmQzIiwicyI6Ik1qVTRNalJqWXpVdFkyWmxOaTAwWVdGa0xXRXpaREl0TlRjeVpUQTVZbU15TlROaCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 20001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'cf.zhetengsha.eu.org';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'zhaox';                        // 节点名称
+const NAME = process.env.NAME || 'zhao';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
